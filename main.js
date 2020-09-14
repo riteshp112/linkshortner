65ff9e1c2935fc515afe5f07ebb67410d86024fa
riteshp112 <50243841+riteshp112@users.noreply.github.com>
2020-09-14 13:44:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function goPython(){
             $.ajax({
-              url:"my.py",
+             source:"my.py",
              context:document.body
             }).done(function() {
              alert('finished python script');
