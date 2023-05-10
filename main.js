aa35947f7f7153c39950bcb67aacf39f44562a31
Ritesh <ritesh.patel@daffodilsw.com>
2023-05-10 18:11:44 +0530

    changes

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function send_request(url) {
 let hashh = window.location.hash;
 let data;
 if (window.location.hash != "") {
-  $.getJSON(endpoint + "/" + hashh, function (data) {
+  $.getJSON(endpoint + "/#" + hashh, function (data) {
     console.log(data);
     data = data["result"];
   });
