fab3fd8fe856e82220e58405be9cb03359a4df3c
Ritesh <ritesh.patel@daffodilsw.com>
2023-05-10 17:43:09 +0530

    hello

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
-var endpoint="https:riteshp112.github.io/linkshortner"
-function getrandom(){
-    var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
-    return random_string();
+let endpoint = "http://riteshp112.github.io/linkshortner";
+function getrandom() {
+  let random_string =
+    Math.random().toString(32).substring(2, 5) +
+    Math.random().toString(32).substring(2, 5);
+  return random_string;
 }
-function geturl(){
-     var url = document.getElementById(“urlinput”).value;
-     var protocol_ok = url.startsWith(“http://”) || url.startsWith(“https://”) || url.startsWith(“ftp://”);
-     if(!protocol_ok){
-         newurl = “http://”+url;
-         return newurl;
-     }
-     else{
-         return url;
-     }
-function genhash(){
-    if (window.location.hash == “”){
-        window.location.hash = getrandom();
-    }}
-function shorturl(){
-    var longurl = geturl();
-    genhash();
-    send_request(longurl);
-    }
+function geturl() {
+  let url = document?.getElementById("urlinput")?.value || "";
+  let protocol_ok =
+    url.startsWith("http://") ||
+    url.startsWith("https://") ||
+    url.startsWith("ftp://");
+  let newurl;
+  if (!protocol_ok) {
+    newurl = "http://" + url;
+    return newurl;
+  } else {
+    return url;
+  }
+}
+function genhash() {
+  if (window.location.hash == "") {
+    window.location.hash = getrandom();
+  }
+}
+function shorturl() {
+  let longurl = geturl();
+  genhash();
+  send_request(longurl);
+}
 function send_request(url) {
-    this.url = url;
-    $.ajax({
-        ‘url’: endpoint + “/” + window.location.hash.substr(1),
-        ‘type’: ‘POST’,
-        ‘data’: JSON.stringify(this.url),
-        ‘dataType’: ‘json’,
-        ‘contentType’: ‘application/json; charset=utf-8’
-    })}
+  this.url = url;
+  $.ajax({
+    url: endpoint + "/" + window.location.hash.substr(1),
+    type: "POST",
+    data: JSON.stringify(this.url),
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+  });
+}
 
-var hashh = window.location.hash.substr(1)
+let hashh = window.location.hash.substr(1);
+let data;
 if (window.location.hash != "") {
-    $.getJSON(endpoint + "/" + hashh, function (data) {
-        data = data["result"];
-    }
-if (data != null) {
-            window.location.href = data;
-        }
+  $.getJSON(endpoint + "/" + hashh, function (data) {
+    data = data["result"];
+  });
+  console.log(data);
+  if (data != null) {
+    window.location.href = data;
+  }
+}
